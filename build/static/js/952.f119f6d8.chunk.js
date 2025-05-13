af500219d44890ea55fecf21afe5e7e21b9063f1
kmaclip <kmartclips@proton.me>
2025-05-12 21:44:54 -0400

    Prepare project for Netlify deployment with SPA routing

---
+++ build/static/js/952.f119f6d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwarehouse_inventory_manager=self.webpackChunkwarehouse_inventory_manager||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.f119f6d8.chunk.js.map
